9ccbb9577b31d7faa1677ec55304df548e72fbb4
Igor Encarnação Junqueira Castro <igorjunqueira19@gmail.com>
2024-07-26 14:44:50 -0300

    fix: Tirando o gitignore

---
+++ Uff-Materiais/.angular/cache/17.0.5/vite/deps/@angular_material_select.js
@@ -0,0 +1,37 @@
+import {
+  MAT_SELECT_CONFIG,
+  MAT_SELECT_SCROLL_STRATEGY,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY,
+  MAT_SELECT_TRIGGER,
+  MatSelect,
+  MatSelectChange,
+  MatSelectModule,
+  MatSelectTrigger,
+  matSelectAnimations
+} from "./chunk-IF7JLQDZ.js";
+import "./chunk-MOGAFKLS.js";
+import "./chunk-2F76VD5W.js";
+import "./chunk-C4GK2VQZ.js";
+import "./chunk-KJXCFBCP.js";
+import "./chunk-Z245CLQL.js";
+import "./chunk-VAG6TXGE.js";
+import "./chunk-ALTC63KX.js";
+import "./chunk-LAGQIDFX.js";
+import "./chunk-DIPPOSFG.js";
+import "./chunk-ER3K3KHR.js";
+import "./chunk-JGL6IZFY.js";
+import "./chunk-V4QF72PL.js";
+export {
+  MAT_SELECT_CONFIG,
+  MAT_SELECT_SCROLL_STRATEGY,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY,
+  MAT_SELECT_TRIGGER,
+  MatSelect,
+  MatSelectChange,
+  MatSelectModule,
+  MatSelectTrigger,
+  matSelectAnimations
+};
+//# sourceMappingURL=@angular_material_select.js.map
---
+++ Uff-Materiais/dist/uff-materiais/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-TV2A23A5.mjs";import"./chunk-WG4PPRMO.mjs";import"./chunk-247AHIEV.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
